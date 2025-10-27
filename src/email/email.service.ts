e3f47d8a91a610c22aa9676c4af85f89be4fb576
Nika <nikushapanchulidze04@gmail.com>
2025-10-27 12:59:43 +0400

    added improvments

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -21,6 +21,9 @@ export class EmailService {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
             },
+            tls: {
+                rejectUnauthorized: false, // Accept self-signed certificates
+            },
         });
     }
 
